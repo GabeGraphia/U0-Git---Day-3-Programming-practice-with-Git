1a3496a4631d71496f72d11d90ded1288feb4922
GabeGraphia <gabegraphia@icloud.com>
2023-08-30 13:00:15 -0700

    Add closeAccount()

--- Atm.java
+++ Atm.java
@@ -7,12 +7,23 @@ public class Atm {
         map = new HashMap<String, Double>();
     }
 
-    public void openAccount(String AccName, Double deposit) throws Exception {
+    public void openAccount(String accName, Double deposit) throws Exception {
 
-        if (map.containsKey(AccName)) {
+        if (map.containsKey(accName)) {
             throw new Exception("Account already exists dumbass");
         } else {
-            map.put(AccName, deposit);
+            map.put(accName, deposit);
         }
     }
+
+    public void closeAccount(String accName) throws Exception {
+        if (!map.containsKey(accName)) {
+            throw new Exception("you dont have an account broke ass idiot");
+        } else if (map.get(accName) > 0) {
+            throw new Exception("you are too rich, get good");
+        } else {
+            map.remove(accName);
+        }
+    }
+
 }
