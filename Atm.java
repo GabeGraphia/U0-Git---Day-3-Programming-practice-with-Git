0c030eb0798323b383f5623f9daaaf7c46c14504
GabeGraphia <gabegraphia@icloud.com>
2023-08-30 17:28:35 -0700

    Added deposit money and null checking

--- Atm.java
+++ Atm.java
@@ -8,7 +8,8 @@ public class Atm {
     }
 
     public void openAccount(String accName, Double deposit) throws Exception {
-
+        checkIfValid(accName);
+        checkIfValid(deposit);
         if (map.containsKey(accName)) {
             throw new Exception("Account already exists dumbass");
         } else {
@@ -17,6 +18,7 @@ public class Atm {
     }
 
     public void closeAccount(String accName) throws Exception {
+        checkIfValid(accName);
         if (!map.containsKey(accName)) {
             throw new Exception("you dont have an account broke ass idiot");
         } else if (map.get(accName) > 0) {
@@ -27,10 +29,29 @@ public class Atm {
     }
 
     public Double checkBalance(String accName) throws Exception {
+        checkIfValid(accName);
         if (!map.containsKey(accName)) {
             throw new Exception("you dont have an account broke ass idiot");
         } else {
             return map.get(accName);
+        }
+    }
+
+    public Double depositMoney(String accName, Double amount) throws Exception {
+        checkIfValid(accName);
+        checkIfValid(amount);
+        if (map.containsKey(accName)) {
+            map.put(accName, map.get(accName) + amount);
+            return amount;
+        } else {
+            throw new Exception("no account");
+        }
+
+    }
+
+    private void checkIfValid(Object obj) throws Exception {
+        if (obj == null) {
+            throw new Exception("WTF?");
         }
     }
 
